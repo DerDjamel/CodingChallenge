e4fd9f73e81d488f365c83375fcf952b8cabd69b
Dermamche Djamel <dermachedjamel@gmail.com>
2023-01-22 06:51:43 +0100

    moved the Redux provider to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,22 @@
 import Cart from './components/Cart';
 import NavigationBar from './components/NavigationBar';
 import Product from './components/Product';
-import { Provider } from 'react-redux';
-import { store } from './store';
 import ProductsList from './components/ProductsList';
 
 function App() {
   return (
     <div className="App">
       <NavigationBar />
-      <Provider store={store}>
-        <main className="container mx-auto mt-5">
-          <div className="flex justify-between">
-            <div className="basis-1/2 p-12">
-              <ProductsList />
-            </div>
-            <div className="basis-1/2 p-4">
-              <Cart />
-            </div>
+      <main className="container mx-auto mt-5">
+        <div className="flex justify-between">
+          <div className="basis-1/2 p-12">
+            <ProductsList />
           </div>
-        </main>
-      </Provider>
+          <div className="basis-1/2 p-4">
+            <Cart />
+          </div>
+        </div>
+      </main>
     </div>
   );
 }
